6adb1ce6f05ac98efb9b3c502b8d69154c7e29e4
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2026-05-26 14:38:34 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd "$DIR"
+cd "$DIR/app"
 
 uv venv --clear
 source .venv/bin/activate
-uv pip install -r ./app/requirements.txt
-python3 ./app/app.py &
+uv pip install -r requirements.txt
+python3 app.py &
 sleep 1
 open http://localhost:8000/
 wait
